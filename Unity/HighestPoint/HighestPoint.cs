aa899952583801e1e8024003e892eaa572072d03
Matthew Hughes <matt.hughes@live.com>
2020-09-08 14:48:15 +1000

    august-changes, sort later

--- Unity/HighestPoint/HighestPoint.cs
+++ Unity/HighestPoint/HighestPoint.cs
@@ -1,22 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-namespace Eidetic.URack.Collection
-{
-    public class HighestPoint : VFXModule
-    {
-        public PointCloud PointCloudOutput { get; set; }
+// namespace Eidetic.URack.Collection
+// {
+//     public class HighestPoint : VFXModule
+//     {
+//         public PointCloud PointCloudOutput { get; set; }
 
-        override public void OnSetPointCloud(PointCloud value)
-        {
-            // Map 1 metre per volt
-            var height = -99f;
-            foreach (var point in value.Points) {
-                if (point.Position.z > height) height = point.Position.z;
-            }
-            Osc.Server.Send(InstanceAddress + "/" + "Height", height);
-            PointCloudOutput = value;
-        }
-    }
-}
+//         override public void OnSetPointCloud(PointCloud value)
+//         {
+//             // Map 1 metre per volt
+//             var height = -99f;
+//             foreach (var point in value.Points) {
+//                 if (point.Position.z > height) height = point.Position.z;
+//             }
+//             Osc.Server.Send(InstanceAddress + "/" + "Height", height);
+//             PointCloudOutput = value;
+//         }
+//     }
+// }
